3e7edb95bbc93b1e9e9c1facf23b079476fb7d39
Dominik Mocher <dominik.mocher@student.tugraz.at>
2014-06-29 22:40:33 +0200

    committing Daniel's new StartDialog due to git problems

--- pandora/Pandora/src/pandora/StartDialog.java
+++ pandora/Pandora/src/pandora/StartDialog.java
@@ -6,9 +6,7 @@
 
 package pandora;
 
-import java.io.File;
 import javax.swing.JDialog;
-import javax.swing.JFileChooser;
 
 /**
  *
@@ -17,15 +15,13 @@ import javax.swing.JFileChooser;
 public class StartDialog extends javax.swing.JDialog {
 
     JDialog gandalf;
-    Game actualGame;
     /**
      * Creates new form StartDialog
      */
-    public StartDialog(java.awt.Frame parent, boolean modal, JDialog wizard, Game game) {
+    public StartDialog(java.awt.Frame parent, boolean modal, JDialog wizard) {
         super(parent, modal);
         initComponents();
         gandalf = wizard;
-        actualGame = game;
     }
 
     /**
@@ -39,89 +35,119 @@ public class StartDialog extends javax.swing.JDialog {
 
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        NewProject = new javax.swing.JButton();
-        LoadProject = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setResizable(false);
 
+        jLabel1.setForeground(new java.awt.Color(0, 102, 255));
         jLabel1.setText("Hallo zum Tabletop-Editor");
 
         jLabel2.setText("Bitte treffen Sie eine Auswahl");
 
-        NewProject.setText("Neues Projekt");
-        NewProject.addActionListener(new java.awt.event.ActionListener() {
+        jButton1.setText("Neues Projekt");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                NewProjectActionPerformed(evt);
+                jButton1ActionPerformed(evt);
             }
         });
 
-        LoadProject.setText("Projekt laden");
-        LoadProject.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                LoadProjectActionPerformed(evt);
-            }
-        });
+        jButton2.setText("Projekt laden");
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/pandora/pandora.jpg"))); // NOI18N
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(118, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1)
-                    .addComponent(jLabel2))
-                .addGap(99, 99, 99))
             .addGroup(layout.createSequentialGroup()
-                .addGap(146, 146, 146)
+                .addComponent(jLabel3)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(LoadProject, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(NewProject))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(27, 27, 27)
+                        .addComponent(jLabel1)
+                        .addContainerGap(21, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
+                            .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jButton1))
+                        .addContainerGap())))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(41, 41, 41)
+                .addComponent(jLabel3)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(34, 34, 34)
                 .addComponent(jLabel2)
-                .addGap(18, 18, 18)
-                .addComponent(NewProject)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(LoadProject)
-                .addContainerGap(133, Short.MAX_VALUE))
+                .addComponent(jButton1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jButton2)
+                .addGap(29, 29, 29))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void NewProjectActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_NewProjectActionPerformed
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
        gandalf.setVisible(true);
-       dispose();
-    }//GEN-LAST:event_NewProjectActionPerformed
+    }//GEN-LAST:event_jButton1ActionPerformed
 
-    private void LoadProjectActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_LoadProjectActionPerformed
-        JFileChooser fc = new JFileChooser();
-        File file = null;
-        String fileName = "";
-        
-        if(fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
-        {
-            file = fc.getSelectedFile();
-            fileName = file.getAbsolutePath().toString();
+    /**
+     * @param args the command line arguments
+     */
+/*    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+/*        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(StartDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(StartDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(StartDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(StartDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        
-        actualGame.loadProject(fileName);
-        dispose();
-    }//GEN-LAST:event_LoadProjectActionPerformed
+        //</editor-fold>
 
+        /* Create and display the dialog */
+  /*      java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                StartDialog dialog = new StartDialog(new javax.swing.JFrame(), true, JDialog wizard);
+                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                    @Override
+                    public void windowClosing(java.awt.event.WindowEvent e) {
+                        System.exit(0);
+                    }
+                });
+                dialog.setVisible(true);
+            }
+        });
+    }*/
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton LoadProject;
-    private javax.swing.JButton NewProject;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     // End of variables declaration//GEN-END:variables
 }
